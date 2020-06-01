50bbed4e77dd4227c9edf2d788f8bacf710284db
belahdj <kaci_hadji@yahoo.fr>
2020-06-01 20:16:17 +0200

    Handle notification

--- src/Loading.js
+++ src/Loading.js
@@ -4,9 +4,16 @@ import { connect } from "react-redux";
 import * as PropTypes from "prop-types";
 import axios from "axios";
 import NavigationService from "./Utils/NavigationService";
-import { navigate } from "./Utils/Account";
 import { getLiveVideo } from "./store/reducers/liveVideoRedux";
 import Notifications from "./services/Notifications";
+import { storeTokenDevice } from "./store/reducers/accountRedux";
+import { logout } from "./store/reducers/authenticationRedux";
+import {
+  ACTIVE_USER,
+  END_LIVE,
+  NEW_LIVE,
+} from "./Utils/Constants/Notifications";
+import NotificationHandler from "./Utils/NotificationHandler";
 
 const styles = StyleSheet.create({
   container: {
@@ -17,31 +24,53 @@ const styles = StyleSheet.create({
 });
 
 class Loading extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   componentDidMount() {
+    NavigationService.setInstance(this.props.navigation);
+    this.notification = new Notifications(
+      this.onRegister.bind(this),
+      this.onNotification.bind(this)
+    );
+
     if (this.props.account && this.props.account.access_token) {
       axios.defaults.headers.Authorization = `Bearer ${this.props.account.access_token}`;
-      if (!this.props.video) {
-        this.props.getLiveVideo();
-      }
+      this.props.getLiveVideo(this.props.account);
+    } else {
+      this.props.navigation.navigate("Login");
     }
-    NavigationService.setInstance(this.props.navigation);
-    navigate(
-      this.props.account,
-      this.props.navigation,
-      "Login",
-      this.props.video && this.props.video.youtube_id
-    );
-    new Notifications(this.onRegister.bind(this));
   }
 
   onRegister = (token) => {
-    console.log('######################## token : ', token);
+    if (!this.props.tokenDevice) {
+      this.props.storeTokenDevice(token);
+    }
+  };
+
+  onNotification = (notification) => {
+    const action = notification.action
+      ? notification.action
+      : notification.data.action;
+
+    if (action === ACTIVE_USER) {
+      this.props.logout();
+    }
+    if (action === NEW_LIVE || action === END_LIVE) {
+      this.props.getLiveVideo(this.props.account);
+    }
+
+    if (!notification.foreground) {
+      NotificationHandler(this.props.navigation, action);
+    }
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Loading</Text>
+        <Text>Chargement</Text>
         <ActivityIndicator size="large" />
       </View>
     );
@@ -49,23 +78,27 @@ class Loading extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-  const { video } = state.liveVideoStore;
+  const { tokenDevice } = state.accountStore;
   return {
     account: state.accountStore,
-    video,
+    tokenDevice,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    getLiveVideo: () => dispatch(getLiveVideo()),
+    getLiveVideo: (account) => dispatch(getLiveVideo(account)),
+    storeTokenDevice: (tokenDevice) => dispatch(storeTokenDevice(tokenDevice)),
+    logout: () => dispatch(logout()),
   };
 };
 Loading.propTypes = {
   account: PropTypes.object,
   navigation: PropTypes.object,
-  video: PropTypes.object,
   getLiveVideo: PropTypes.func,
+  storeTokenDevice: PropTypes.func,
+  tokenDevice: PropTypes.object,
+  logout: PropTypes.func,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Loading);
